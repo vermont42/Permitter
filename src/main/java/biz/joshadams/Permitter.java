10bff1b57af44842456b7029fd39a0d837a9e29a
vermont42 <vermontlawyer@gmail.com>
2017-12-31 12:54:38 -0800

    dynamically choose day

--- src/main/java/biz/joshadams/Permitter.java
+++ src/main/java/biz/joshadams/Permitter.java
@@ -7,14 +7,42 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.*;
 
 public class Permitter {
     private static String username;
     private static String password;
+    private static int permitMonthsAhead;
+    private static int permitDayOfMonth;
 
     public static void main(String[] args) {
+        setupTime();
         loadCredentials();
         getPermit();
+    }
+
+    private static void setupTime() {
+        final int daysAhead = 60;
+        LocalDate currentDate = LocalDate.now();
+        LocalDate futureDate = currentDate.plusDays(daysAhead);
+        DayOfWeek futureDayOfWeek = futureDate.getDayOfWeek();
+        if (futureDayOfWeek == DayOfWeek.SATURDAY || futureDayOfWeek == DayOfWeek.SUNDAY) {
+            System.out.println("Did not purchase permit because " + daysAhead + " from now is on a weekend.");
+            System.exit(0);
+        }
+        // TODO: If futureDate is a holiday, exit without purchasing.
+        permitDayOfMonth = futureDate.getDayOfMonth();
+        int normalizedCurrentMonth = currentDate.getMonthValue();
+        int normalizedFutureMonth = futureDate.getMonthValue();
+        if (normalizedCurrentMonth > normalizedFutureMonth) {
+            normalizedCurrentMonth -= 12;
+        }
+        else if (normalizedFutureMonth < normalizedCurrentMonth) {
+            normalizedFutureMonth += 12;
+        }
+        permitMonthsAhead = normalizedFutureMonth - normalizedCurrentMonth;
+
+        System.out.println("ahead " + permitMonthsAhead + "  day: " + permitDayOfMonth);
     }
 
     private static void loadCredentials() {
@@ -47,19 +75,21 @@ public class Permitter {
         driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.id("submit")).click();
         driver.findElement(By.linkText("Select")).click();
-        driver.findElement(By.id("type_id_34")).click(); // 34: RockRidge  37: Orinda
+        driver.findElement(By.id("type_id_37")).click(); // 34: RockRidge  37: Orinda
         driver.findElement(By.xpath("//input[@value='Next']")).click();
-        driver.findElement(By.xpath("//a[2]/span")).click();
-        driver.findElement(By.xpath("//a[2]/span")).click();
-        driver.findElement(By.xpath("//a[text() = '4']")).click(); // Day: 4
-        driver.findElement(By.xpath("//tr[2]/td/div/div/div/a[2]/span")).click();
-        driver.findElement(By.xpath("//tr[2]/td/div/div/div/a[2]/span")).click();
-        driver.findElement(By.xpath("(//a[text() = '4'])[2]")).click(); // Day: 4
+        for (int i = 0; i < permitMonthsAhead; i++) {
+            driver.findElement(By.xpath("//a[2]/span")).click();
+        }
+        driver.findElement(By.xpath("//a[text() = '" + permitDayOfMonth + "']")).click();
+        for (int i = 0; i < permitMonthsAhead; i++) {
+            driver.findElement(By.xpath("//tr[2]/td/div/div/div/a[2]/span")).click();
+        }
+        driver.findElement(By.xpath("(//a[text() = '" + permitDayOfMonth + "'])[2]")).click();
         driver.findElement(By.xpath("//input[@value='Next']")).click();
         driver.findElement(By.xpath("//input[@value='Next']")).click();
         driver.findElement(By.xpath("(//input[@value='Next'])[2]")).click();
         driver.findElement(By.id("conditions")).click();
-        driver.findElement(By.id("complete_order")).click();
+        //driver.findElement(By.id("complete_order")).click();
     }
 
     private static void sleep() {
